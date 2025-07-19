7789a02f5bc225894a9fab8832233345b51f03c8
Guru Lakshana <166280039+Gurulakshana@users.noreply.github.com>
2025-07-19 14:07:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 var ul=document.querySelector("table")
-let Userdata=''
+n=JSON.parse(localStorage.getItem("n"))
 function add()
 {
     var list=document.createElement("tr")
     var inp=document.getElementById("inp")
-    list.innerHTML="<td><input onclick='check(event)' type='checkbox'></td>"+`<td id='td1'><a id='a'>${inp.value}</a></td>`+"<td><button id='div1' onclick='del(event)'>x</button></td>"
+    list.innerHTML="<tr><td><input onclick='check(event)' type='checkbox'></td>"+`<td id='td1'><a id='a'>${inp.value}</a></td>`+"<td><button id='div1' onclick='del(event)'>x</button></td></tr>"
     ul.append(list)
     save() //saving after additiond
 }
-if(table.value==undefined){
-    displayData(); //when we open newly the last input value on last open will be displayed
-    function displayData(){
-        const s=localStorage.getItem("Userdata") //retriving data
-        document.getElementById("table").innerHTML=s //dispaying
-}
+function displayData(){
+        let users = JSON.parse(localStorage.getItem("user")) //retriving data
+        document.getElementById("table").innerHTML=users[n].todo //dispaying
 }
 function save(){
     inp.value="" //to make input field after entering data
-    Userdata+=localStorage.setItem("Userdata",table.innerHTML) //to store input value and display on next open
+    let users = JSON.parse(localStorage.getItem("user"))
+    users[n].todo=table.innerHTML
+    localStorage.setItem("user",JSON.stringify(users))
 }
         
     
